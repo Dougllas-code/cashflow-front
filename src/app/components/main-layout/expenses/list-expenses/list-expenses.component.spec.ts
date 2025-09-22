46e353a2b05ca9b4de5e7a437afd4f5594befdab
Dougllas Souza <dougllas58@outlook.com>
2025-09-22 12:08:46 -0300

    fix: unit test fixed

--- src/app/components/main-layout/expenses/list-expenses/list-expenses.component.spec.ts
+++ src/app/components/main-layout/expenses/list-expenses/list-expenses.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ListExpensesComponent } from './list-expenses.component';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ListExpensesComponent', () => {
   let component: ListExpensesComponent;
@@ -8,7 +10,11 @@ describe('ListExpensesComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ListExpensesComponent]
+      imports: [ListExpensesComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
     })
     .compileComponents();
 
